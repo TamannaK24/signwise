8119adc0e862888d6f0bf8fc6d3e09ba9a7fb873
Aima <aimasalman404@gmail.com>
2025-02-17 10:55:02 -0600

    made navbar and basic pages

---
+++ signwise/src/pages/Features.jsx
@@ -0,0 +1,10 @@
+function Features() {
+
+    return (
+      <div className="features">
+        <h1>Features</h1>
+      </div>
+    );
+  };
+  
+  export default Features;
---
+++ signwise/src/pages/Learn.jsx
@@ -0,0 +1,10 @@
+function Learn() {
+
+    return (
+      <div className="learn">
+        <h1>Learn</h1>
+      </div>
+    );
+  };
+  
+  export default Learn;
