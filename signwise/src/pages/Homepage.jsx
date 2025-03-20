8e6941aef9b2f042ce7a1a10fa2b0b0fbc2dcf5f
Tamanna Khurana <Tamanna122404@gmail.com>
2025-03-20 00:16:03 -0500

    fixed the css for the homepage

--- signwise/src/pages/Homepage.jsx
+++ signwise/src/pages/Homepage.jsx
@@ -1,22 +1,20 @@
 import '../Homepage.css'
 
 function Homepage() {
-
   return (
     <div className="homepage">
       <div className="homepage-text">
-        <div className="container">
+        <div className="text-section">
           <p>SignWise</p>
           <p>Your ASL Learning</p>
           <p>Companion</p>
-          <br></br>
-          <button className ="get-started-btn" type="button">Get Started</button>
         </div>
-        
+        <div className="button-section">
+          <button className="get-started-btn" type="button">Get Started</button>
+        </div>
       </div>
     </div>
-    
   );
-};
+}
 
 export default Homepage;
