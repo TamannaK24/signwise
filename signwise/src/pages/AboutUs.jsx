e4fd17f34d22d32a79e97fd836b37167061b63af
Aima <aimasalman404@gmail.com>
2025-03-21 17:39:14 -0500

    it's not wonky anymoregit add .

--- signwise/src/pages/AboutUs.jsx
+++ signwise/src/pages/AboutUs.jsx
@@ -5,26 +5,32 @@ function AboutUs() {
 
     return (
       <>
+      <h1>About Us</h1>
       <div className="row">
-        <h1>About Us</h1>
         <div className="column">
           <h2>Clear</h2>
-          <p>Many ASL tools struggle with reading correct signs, 
+          <p>
+            Many ASL tools struggle with reading correct signs, 
             leading to ambiguity. Our advanced model refines translations
              to create an easy learning experience, making ASL learning more 
-             intuitive and reliable.</p>
+             intuitive and reliable.
+          </p>
         </div>
         <div className="column">
-        <h2>Bridging the Gap</h2>
-          <p>Unlike other solutions that may lack responsiveness or structured 
+          <h2>Bridging the Gap</h2>
+          <p>
+            Unlike other solutions that may lack responsiveness or structured 
             content, our comprehensive curriculum helps users grasp ASL more effectively
-             while understanding its nuances.</p>
+             while understanding its nuances.
+          </p>
         </div>
         <div className="column">
-        <h2>Accessible</h2>
-          <p>By offering this tool free of cost to diverse demographics and multiple 
+          <h2>Accessible</h2>
+          <p>
+            By offering this tool free of cost to diverse demographics and multiple 
             language users, we aim to break down communication barriers and promote ASL 
-            awareness.</p>
+            awareness.
+          </p>
         </div>
       </div>
       <div className="learn-btn">
