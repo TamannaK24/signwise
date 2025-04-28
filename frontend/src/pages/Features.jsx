78406b318cd45e78a26fb1beff374249c85c8479
Aima <aimasalman404@gmail.com>
2025-04-28 15:47:43 -0500

    fixed merge conflicts

--- frontend/src/pages/Features.jsx
+++ frontend/src/pages/Features.jsx
@@ -2,15 +2,15 @@ import '../Features.css';
 
 function Features() {
 
-    return (
-      <>
+  return (
+    <>
       <h1>Features</h1>
       <div className="row">
         <div className="column">
-          <h2>Personalized Dashboard</h2>
+          <h2 className="subtitle">Personalized Dashboard</h2>
           <p>
-            Easily track your progress with a clear, user-friendly dashboard. 
-            View upcoming lessons, check completed milestones, and stay focused 
+            Easily track your progress with a clear, user-friendly dashboard.
+            View upcoming lessons, check completed milestones, and stay focused
             with an overview of everything you're working toward.
           </p>
         </div>
@@ -23,19 +23,19 @@ function Features() {
           <img src="https://placehold.co/500x300" alt="Feature 2" />
         </div>
         <div className="column">
-          <h2>Interactive Lessons</h2>
+          <h2 className="subtitle">Interactive Lessons</h2>
           <p>
-            Learn ASL through engaging exercises, quizzes, and practice sessions 
-            designed to build real-world communication skills. Every lesson adapts 
+            Learn ASL through engaging exercises, quizzes, and practice sessions
+            designed to build real-world communication skills. Every lesson adapts
             to your pace, helping you stay confident and consistent.
           </p>
         </div>
       </div>
       <div className="row">
         <div className="column">
-          <h2>Progress Tracking</h2>
+          <h2 className="subtitle">Progress Tracking</h2>
           <p>
-            Set and customize your own learning goals to stay motivated. Whether it's mastering a 
+            Set and customize your own learning goals to stay motivated. Whether it's mastering a
             new set of signs each week or completing a course, your goals grow with you.
             Monitor your learning journey with detailed progress reports.
           </p>
